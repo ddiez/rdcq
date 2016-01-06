9008447d292e7d8695d303f6ae17f86dfeff1eb1
Diego Diez <diego10ruiz@gmail.com>
2016-01-06 19:15:58 +0900

    Added zoom interactivity.

--- app.R
+++ app.R
@@ -33,13 +33,24 @@ ui <- shinyUI({
                     label = "Filter:",
                     value = ""
           ),
-          plotOutput("cells")
+          plotOutput("cells",
+                     dblclick = "cells_dblclick",
+                     brush = brushOpts(
+                       id = "cells_brush",
+                       resetOnNew = TRUE
+                     ))
         ),
         tabPanel(title = "Markers",
           dataTableOutput("markers")
         ),
         tabPanel(title = "Expression",
-                 plotOutput("expression")
+                 plotOutput("expression",
+                            dblclick = "expression_dblclick",
+                            brush = brushOpts(
+                              id = "expression_brush",
+                              resetOnNew = TRUE
+                            )
+                            )
                  )
       )
     )
@@ -47,7 +58,9 @@ ui <- shinyUI({
 })
 server <- shinyServer(function(input, output, server) {
   source("DCQ.R")
+  dbd <- melt(db, varnames = c("marker", "celltype"))
   library(limma)
+  library(plotly)
 
     file <- reactive({
     if(! is.null(input$file$name)) {
@@ -72,19 +85,43 @@ server <- shinyServer(function(input, output, server) {
     if(!is.null(d))
       d %>% dplyr::filter(grepl(input$filter, celltype, ignore.case = TRUE))
   })
+  
+  ranges_cells <- reactiveValues(x = NULL, y = NULL)
   output$cells <- renderPlot({
     d <- filter()
     if(!is.null(d))
-      ggplot(d,aes(x = sample, y = celltype,fill = value)) + geom_tile() + scale_fill_gradient2(low = "seagreen", mid = "white", high = "purple4", midpoint = 0, limit = c(-.12,.12)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5), axis.text.y = element_text(size = 10))
-    else
-      print("ready.")
+      ggplot(d,aes(x = sample, y = celltype,fill = value)) + geom_tile() + scale_fill_gradient2(low = "seagreen", mid = "white", high = "purple4", midpoint = 0, limit = c(-.12,.12)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5), axis.text.y = element_text(size = 10)) + coord_cartesian(xlim = ranges_cells$x, ylim = ranges_cells$y)
   }, width = 500, height = 500)
+  
+  observeEvent(input$cells_dblclick, {
+    brush <- input$cells_brush
+    if (!is.null(brush)) {
+      ranges_cells$x <- c(brush$xmin, brush$xmax)
+      ranges_cells$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges_cells$x <- NULL
+      ranges_cells$y <- NULL
+    }
+  })
   
   output$markers <- renderDataTable(markers)
   
+  ranges_expression <- reactiveValues(x = NULL, y = NULL)
   output$expression <- renderPlot({
-    d <- melt(db, varnames = c("marker", "celltype"))
-    ggplot(d,aes(x=celltype,y=marker,fill=value)) + geom_tile() + scale_fill_gradientn(colors = .jet.colors(128)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+    ggplot(dbd,aes(x=celltype,y=marker,fill=value)) + geom_tile() + scale_fill_gradientn(colors = .jet.colors(128)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5)) + coord_cartesian(xlim = ranges_expression$x, ylim = ranges_expression$y)
+  })
+  
+  observeEvent(input$expression_dblclick, {
+    brush <- input$expression_brush
+    if (!is.null(brush)) {
+      ranges_expression$x <- c(brush$xmin, brush$xmax)
+      ranges_expression$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges_expression$x <- NULL
+      ranges_expression$y <- NULL
+    }
   })
 })
 shinyApp(ui = ui, server = server)
