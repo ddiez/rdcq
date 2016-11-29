85d43b6ef8ec9dd29b6c52a712920be173429a33
Diego Diez <diego10ruiz@gmail.com>
2016-11-29 14:03:23 +0900

    Use plotly for interactive heatmaps.

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(reshape2)
 library(glmnet)
 library(limma)
+library(plotly)
 source("DCQ.R")
 
 ui <- shinyUI({
@@ -33,34 +34,19 @@ ui <- shinyUI({
                 value = 100
                 )
     ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel(title = "Cell quantification",
-          textInput("filter",
-                    label = "Filter:",
-                    value = ""
-          ),
-          plotOutput("cells",
-                     dblclick = "cells_dblclick",
-                     brush = brushOpts(
-                       id = "cells_brush",
-                       resetOnNew = TRUE
-                     ))
-        ),
-        tabPanel(title = "Markers",
-          dataTableOutput("markers")
-        ),
-        tabPanel(title = "Expression",
-                 plotOutput("expression",
-                            dblclick = "expression_dblclick",
-                            brush = brushOpts(
-                              id = "expression_brush",
-                              resetOnNew = TRUE
-                            )
-                            )
-                 )
-      )
-    )
+    mainPanel(tabsetPanel(
+      tabPanel(
+        title = "Cell quantification",
+        textInput("filter",
+                  label = "Filter:",
+                  value = ""),
+        plotlyOutput("cells")
+      ),
+      tabPanel(title = "Markers",
+               dataTableOutput("markers")),
+      tabPanel(title = "Expression",
+               plotlyOutput("expression"))
+    ))
   )
 })
 server <- shinyServer(function(input, output, server) {
@@ -98,41 +84,33 @@ server <- shinyServer(function(input, output, server) {
       d %>% dplyr::filter(grepl(input$filter, celltype, ignore.case = TRUE))
   })
   
-  ranges_cells <- reactiveValues(x = NULL, y = NULL)
-  output$cells <- renderPlot({
+  output$cells <- renderPlotly({
     d <- filter()
-    if(!is.null(d))
-      ggplot(d,aes(x = sample, y = celltype,fill = value)) + geom_tile() + scale_fill_gradient2(low = "seagreen", mid = "white", high = "purple4", midpoint = 0, limit = c(-.12,.12)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5), axis.text.y = element_text(size = 10)) + coord_cartesian(xlim = ranges_cells$x, ylim = ranges_cells$y)
-  }, width = 500, height = 500)
-  
-  observeEvent(input$cells_dblclick, {
-    brush <- input$cells_brush
-    if (!is.null(brush)) {
-      ranges_cells$x <- c(brush$xmin, brush$xmax)
-      ranges_cells$y <- c(brush$ymin, brush$ymax)
-      
-    } else {
-      ranges_cells$x <- NULL
-      ranges_cells$y <- NULL
+    if (!is.null(d)) {
+      p <-
+        ggplot(d, aes(x = sample, y = celltype, fill = value)) + 
+        geom_tile() + 
+        scale_fill_gradient2(
+          low = "seagreen",
+          mid = "white",
+          high = "purple4",
+          midpoint = 0,
+          limit = c(-.12, .12)
+        ) + 
+        theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+      ggplotly(p)
     }
   })
   
   output$markers <- renderDataTable(markers)
   
-  ranges_expression <- reactiveValues(x = NULL, y = NULL)
-  output$expression <- renderPlot({
-    ggplot(dbd,aes(x=celltype,y=marker,fill=value)) + geom_tile() + scale_fill_gradientn(colors = .jet.colors(128)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5)) + coord_cartesian(xlim = ranges_expression$x, ylim = ranges_expression$y)
-  })
-  
-  observeEvent(input$expression_dblclick, {
-    brush <- input$expression_brush
-    if (!is.null(brush)) {
-      ranges_expression$x <- c(brush$xmin, brush$xmax)
-      ranges_expression$y <- c(brush$ymin, brush$ymax)
-      
-    } else {
-      ranges_expression$x <- NULL
-      ranges_expression$y <- NULL
+  output$expression <- renderPlotly({
+    if (!is.null(dbd)) {
+      p <- ggplot(dbd, aes(x = celltype, y = marker, fill = value)) + 
+        geom_tile() + 
+        scale_fill_gradient2(low = "blue", mid = "yellow", high = "red", midpoint = 4) + 
+        theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+      ggplotly(p) 
     }
   })
 })
