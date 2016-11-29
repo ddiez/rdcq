984c0d4498ae8a1045c9150a54dfb5fa87690a69
Diego Diez <diego10ruiz@gmail.com>
2016-11-29 15:59:46 +0900

    Created package structure. Code reorganization and clean up.

---
+++ R/help.R
@@ -0,0 +1,10 @@
+#' DCQ
+#'
+#' A Shiny Interface to DCQ and Friends
+#'
+#' @name DCQ-package
+#' @aliases DCQ
+#' @docType package
+#'
+#' @importFrom glmnet glmnet
+NULL
---
+++ inst/shiny/app.R
@@ -0,0 +1,131 @@
+library(shiny)
+library(readr)
+library(dplyr)
+library(reshape2)
+library(limma)
+library(ggplot2)
+library(plotly)
+library(DCQ)
+
+dbd <- reshape2::melt(db, varnames = c("marker", "celltype"))
+
+ui <- shinyUI({
+  pageWithSidebar(
+    headerPanel(title = "DCQ: Digital Cell Quantifier"),
+    sidebarPanel(
+      fileInput("file",
+                "file:"),
+      sliderInput(
+        "alpha",
+        "alpha:",
+        min = 0,
+        max = 1,
+        value = 0.05,
+        step = 0.01
+      ),
+      sliderInput(
+        "lambda.min.ratio",
+        "lambda.min.ratio:",
+        min = 0,
+        max = 1,
+        value = 0.2,
+        step = 0.01
+      ),
+      textInput("nlambda", "nlambda", value = 100)
+    ),
+    mainPanel(tabsetPanel(
+      tabPanel(
+        title = "Cell quantification",
+        textInput("filter",
+                  label = "Filter:",
+                  value = ""),
+        plotlyOutput("cells")
+      ),
+      tabPanel(title = "Markers",
+               dataTableOutput("markers")),
+      tabPanel(title = "Expression",
+               plotlyOutput("expression"))
+    ))
+  )
+})
+server <- shinyServer(function(input, output, server) {
+  values <- reactiveValues(data = NULL)
+
+  observe({
+    if (!is.null(input$file$name)) {
+      x <-
+        readr::read_delim(
+          file = input$file$datapath,
+          delim = "\t",
+          escape_double = FALSE
+        )
+      x <- limma::avereps(x[, -1], ID = x[, 1] %>% unlist)
+      x <- x[rownames(x) %in% markers[, 2], ]
+      rownames(x) <- markers[rownames(x), 1]
+      values$data <- x
+    }
+  })
+
+  dcq <- eventReactive(values$data, {
+    tmp <- DCQ(
+      values$data,
+      db = db,
+      alpha = input$alpha,
+      lambda.min.ratio = input$lambda.min.ratio,
+      nlambda = input$nlambda
+    )
+    return(reshape2::melt(tmp, varnames = c("sample", "celltype")))
+  })
+
+  filter <- reactive({
+    d <- dcq()
+    if (!is.null(d))
+      d %>% dplyr::filter(grepl(input$filter, celltype, ignore.case = TRUE))
+  })
+
+  output$cells <- renderPlotly({
+    d <- filter()
+    if (!is.null(d)) {
+      p <-
+        ggplot(d, aes(x = sample, y = celltype, fill = value)) +
+        geom_tile() +
+        scale_fill_gradient2(
+          low = "seagreen",
+          mid = "white",
+          high = "purple4",
+          midpoint = 0,
+          limit = c(-.12, .12)
+        ) +
+        theme(axis.text.x = element_text(
+          angle = 90,
+          hjust = 1,
+          vjust = .5
+        ))
+      plotly::ggplotly(p)
+    }
+
+  })
+
+  output$markers <- renderDataTable(markers)
+
+  output$expression <- renderPlotly({
+    if (!is.null(dbd)) {
+      p <- ggplot(dbd, aes(x = celltype, y = marker, fill = value)) +
+        geom_tile() +
+        scale_fill_gradient2(
+          low = "blue",
+          mid = "yellow",
+          high = "red",
+          midpoint = 4
+        ) +
+        theme(axis.text.x = element_text(
+          angle = 90,
+          hjust = 1,
+          vjust = .5
+        ))
+      plotly::ggplotly(p)
+    }
+  })
+})
+
+shinyApp(ui = ui, server = server)
